9302369be60d184a104d171981d5d0fc30848370
ADI SAPUTRA <Adi.Saputra@cimbniaga.co.id>
2022-05-17 18:05:57 +0700

    docs: swagger info

--- src/main/java/id/saputra/adi/productsdataservice/config/SwaggerConfig.java
+++ src/main/java/id/saputra/adi/productsdataservice/config/SwaggerConfig.java
@@ -16,8 +16,8 @@ import java.util.Collections;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private static final String API_TITLE = "REST API Docs for Customer Services";
-    private static final String API_DESC = "REST API for handling Customer Services";
+    private static final String API_TITLE = "REST API Docs for Product Services";
+    private static final String API_DESC = "REST API for handling Product Services";
     private static final String API_VERSION = "v1";
     private static final String API_CONTACT_NAME = "Adi Saputra";
     private static final String API_CONTACT_EMAIL = "adisapu10022@gmail.com";
